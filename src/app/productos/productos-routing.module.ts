fa6ac7bd503f87efdd5bb7d2a888c8c85cf67157
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-09-03 17:00:27 -0600

    lazy load

--- src/app/productos/productos-routing.module.ts
+++ src/app/productos/productos-routing.module.ts
@@ -2,7 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AgregarComponent } from './pages/agregar/agregar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'agregar', component: AgregarComponent
+      },
+      {
+        path: '**', redirectTo: 'agregar'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
